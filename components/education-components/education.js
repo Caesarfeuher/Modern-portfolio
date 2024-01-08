43b906efc51f72a9ba18a3688c53a2e6a26f4729
hydra <hydra@hydras-MacBook-Air-2.local>
2024-01-08 16:25:51 +0100

    fourth

--- components/education-components/education.js
+++ components/education-components/education.js
@@ -37,25 +37,25 @@ const Education = () => {
       <div className="education-item">
         <p className="text-gray-400 text-4xl">Bachelor of Science in Mechanical Engineering</p>
         <h2 className="text-gray-400 text-2xl">2015-2019 | Kwame Nkrumah University Of Science And Technology (KNUST) </h2>
-        <p className="text-gray-400">Mechanical and aerospace Engineering background</p>
+        <h3 className="text-gray-400">Mechanical and aerospace Engineering background</h3>
       </div>
 
       <div className="education-item">
         <p className="text-gray-400 text-4xl">Diploma in Frontend Development</p>
         <h2 className="text-gray-400 text-2xl">2021 | HIIT Plc</h2>
-        <p className="text-gray-400">HTML. CSS. BOOTSTRAP. Tailwind. React. ReactNative. Kotlin. CSS Frameworks. Javascript. Next. Typescript</p>
+        <h3 className="text-gray-400">HTML. CSS. BOOTSTRAP. Tailwind. React. ReactNative. Kotlin. CSS Frameworks. Javascript. Next. Typescript</h3>
       </div>
 
       <div className="education-item">
         <p className="text-gray-400 text-4xl">AWS Cloud Practitioner</p>
         <h2 className="text-gray-400 text-2xl">2022 | ALX</h2>
-        <p className="text-gray-400">Introduction to Cloud Computing:AWS cloud practitioner course</p>
+        <h3 className="text-gray-400">Introduction to Cloud Computing:AWS cloud practitioner course</h3>
       </div>
 
       <div className="education-item">
         <p className="text-gray-400 text-4xl">Fuel/LPG station and Petroleum Depot Management </p>
         <h2 className="text-gray-400 text-2xl">2018 | ASCON OIL</h2>
-        <p className="text-gray-400"> Safety course, gas station operation and management course, petroleum products depot management course</p>
+        <h3 className="text-gray-400"> Safety course, gas station operation and management course, petroleum products depot management course</h3>
       </div>
 
       <style jsx>{`
@@ -82,9 +82,18 @@ const Education = () => {
           .education-item {
             width: 50%; /* Make the width 100% for smaller screens */
             overflow:auto
+            text-align: center;
+            border: 1px solid #ccc;
+            margin-top: 8px;
+            padding: 8px;
           }
           .education-item p {
-            font-size: 2xl !important; /* Add !important to ensure the style is applied */
+            // font-size: 2xl !important; /* Add !important to ensure the style is applied */
+            font-size: 1.5rem;
+          }
+          .education-item h2 {
+            // font-size: 2xl !important; /* Add !important to ensure the style is applied */
+            font-size: 0.9rem;
           }
         }
       `}</style>
